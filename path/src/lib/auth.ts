8891a3703bd02ef1c1b36a4451ef1ddd9583b7a0
matteopoggesi08 <matteopoggesi@gmail.com>
2026-08-22 14:07:32 +0200

    Update auth.ts

--- path/src/lib/auth.ts
+++ path/src/lib/auth.ts
@@ -1,14 +1,10 @@
 import { supabase } from './supabase';
 
-const getRedirectUrl = () => {
-  return `${window.location.origin}/mrsoccergamification/#/`;
-};
-
 export const login = () =>
   supabase.auth.signInWithOAuth({
     provider: 'google',
     options: {
-      redirectTo: getRedirectUrl(),
+      redirectTo: `${window.location.origin}/mrsoccergamification/`,
     },
   });
 
