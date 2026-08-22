4941f40120af3c924cc08123759100ab6a5e5329
matteopoggesi08 <matteopoggesi@gmail.com>
2026-08-22 14:01:56 +0200

    Update auth.ts

--- path/src/lib/auth.ts
+++ path/src/lib/auth.ts
@@ -1,1 +1,19 @@
-import{supabase}from'./supabase';export const login=()=>supabase.auth.signInWithOAuth({provider:'google',options:{redirectTo:location.href.split('#')[0]+'#/'}});export const logout=()=>supabase.auth.signOut();
+import { supabase } from './supabase';
+
+const getRedirectUrl = () => {
+  if (import.meta.env.PROD) {
+    return `${window.location.origin}/mrsoccergamification/#/`;
+  }
+
+  return `${window.location.origin}/#/`;
+};
+
+export const login = () =>
+  supabase.auth.signInWithOAuth({
+    provider: 'google',
+    options: {
+      redirectTo: getRedirectUrl(),
+    },
+  });
+
+export const logout = () => supabase.auth.signOut();
