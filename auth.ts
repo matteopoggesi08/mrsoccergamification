2a58d6d5584da38559980eeb1813ccbc8aabde83
matteopoggesi08 <matteopoggesi@gmail.com>
2026-09-23 14:58:40 +0200

    Delete auth.ts

--- auth.ts
+++
@@ -1,3 +0,0 @@
-import{supabase}from'./supabase';
-export const login=()=>supabase.auth.signInWithOAuth({provider:'google',options:{redirectTo:`${window.location.origin}/mrsoccergamification/`}});
-export const logout=()=>supabase.auth.signOut();
